286076fdb554f5fb228c41594589e951115edad2
shouyinji1 <shouyinji123@gmail.com>
2018-07-23 14:53:35 +0800

    modified a file

--- 自动录入_中国移动淮安高校活动支撑平台.py
+++ 自动录入_中国移动淮安高校活动支撑平台.py
@@ -2,45 +2,52 @@ import requests
 import openpyxl
 from requests_toolbelt import MultipartEncoder
 
-file='excel file'
+login_data={'operator_id':'User','password':'Password'}
+file='File Name'
 sheet='Sheet1'
+min_range=1589
+max_range=1627
+
+
+# API of website
+get_cookies_url='http://183.207.196.70:8085/haywdjpt/login!login.jspa'
+post1_referer_url='http://183.207.196.70:8085/haywdjpt/gxdj/ywdj!checkYwMsisdn.jspa'
+post2_referer_url='http://183.207.196.70:8085/haywdjpt/gxdj/ywdj!saveLogin.jspa'
+test_login='http://183.207.196.70:8085/haywdjpt/gxdj/ywdj!frame.jspa'
+
+#登陆
+session=requests.session()
+session.post(get_cookies_url,data=login_data)
 
 #读取Excel
 def excel_data(file,sheet):
-    wb=openpyxl.load_workbook(filename=file)
-    sheet_ranges=wb[sheet]
-    return(sheet_ranges)
+	wb=openpyxl.load_workbook(filename=file)
+	sheet_ranges=wb[sheet]
+	return(sheet_ranges)
 
-for i in range(2,32):
-    excel=excel_data(file,sheet)
-    if(excel['F'+str(i)].value in ['√']):
-        phoneNumber=str(excel['C'+str(i)].value)
-        print(phoneNumber)
-    else:
-        continue
-    
-    paramater={'msisdn':phoneNumber,'yw_id':'1761','bl_time':'2018-06-08'}
+for i in range(min_range,max_range):
+	excel=excel_data(file,sheet)
+	if(excel['I'+str(i)].value in ['同意']): #筛选条件
+		phoneNumber=str(excel['A'+str(i)].value) #获取号码
+		print(phoneNumber)
+	else:
+		continue
 
-    login_data={'operator_id':'user','password':'password'}
-    multiFiles={
-            'region_id':'1210',
-            'school_id':'295',
-            **paramater,
-            'fzr_id':login_data['operator_id'],
-            'fzr_name':login_data['operator_id']
-        }
+	paramater={
+		'msisdn':phoneNumber,
+		'yw_id':'1761',	# 业务名称
+		'bl_time':'2018-07-21'	# 办理时间
+	}
 
-    # API of website
-    get_cookies_url='http://183.207.196.70:8085/haywdjpt/login!login.jspa'
-    post1_referer_url='http://183.207.196.70:8085/haywdjpt/gxdj/ywdj!checkYwMsisdn.jspa'
-    post2_referer_url='http://183.207.196.70:8085/haywdjpt/gxdj/ywdj!saveLogin.jspa'
-    test_login='http://183.207.196.70:8085/haywdjpt/gxdj/ywdj!frame.jspa'
+	multiFiles={
+		'region_id':'1210',
+		'school_id':'295', # 学校
+		**paramater,
+		'fzr_id':login_data['operator_id']
+	}
 
-    multiFormData=MultipartEncoder(fields=multiFiles)
-    session=requests.session()
-    session.post(get_cookies_url,data=login_data)
-
-    res=session.post(post1_referer_url,data=paramater)
-    if res.text=='0':
-        res=session.post(post2_referer_url,data=multiFormData,headers={'Content-Type':multiFormData.content_type})
-    print(res.text)
+	multiFormData=MultipartEncoder(fields=multiFiles)
+	res=session.post(post1_referer_url,data=paramater)
+	if res.text=='0':
+		res=session.post(post2_referer_url,data=multiFormData,headers={'Content-Type':multiFormData.content_type})
+	print(res.text)
